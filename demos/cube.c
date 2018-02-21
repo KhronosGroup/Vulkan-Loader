708e73797b8a0d0480f437f4672d7e23f0ebe3fc
Mark Young <marky@lunarg.com>
2018-02-21 15:29:41 -0700

    cube: Use string_VkObjectType
    Use the existing string_VkObjectType utility found in
    vk_enum_string_helper.h instead of the manually defined
    DebugAnnotObjectToString function.
    
    Change-Id: I9e9ecfc1c512ec1b9b98cce2f846c565ac98ecbf

--- demos/cube.c
+++ demos/cube.c
@@ -56,6 +56,7 @@
 
 #include <vulkan/vk_sdk_platform.h>
 #include "linmath.h"
+#include "vk_enum_string_helper.h"
 
 #include "gettime.h"
 #include "inttypes.h"
@@ -449,79 +450,6 @@ struct demo {
     uint32_t queue_family_count;
 };
 
-static const char *DebugAnnotObjectToString(const VkObjectType object_type) {
-    switch (object_type) {
-        case VK_OBJECT_TYPE_INSTANCE:
-            return "VkInstance";
-        case VK_OBJECT_TYPE_PHYSICAL_DEVICE:
-            return "VkPhysicalDevice";
-        case VK_OBJECT_TYPE_DEVICE:
-            return "VkDevice";
-        case VK_OBJECT_TYPE_QUEUE:
-            return "VkQueue";
-        case VK_OBJECT_TYPE_SEMAPHORE:
-            return "VkSemaphore";
-        case VK_OBJECT_TYPE_COMMAND_BUFFER:
-            return "VkCommandBuffer";
-        case VK_OBJECT_TYPE_FENCE:
-            return "VkFence";
-        case VK_OBJECT_TYPE_DEVICE_MEMORY:
-            return "VkDeviceMemory";
-        case VK_OBJECT_TYPE_BUFFER:
-            return "VkBuffer";
-        case VK_OBJECT_TYPE_IMAGE:
-            return "VkImage";
-        case VK_OBJECT_TYPE_EVENT:
-            return "VkEvent";
-        case VK_OBJECT_TYPE_QUERY_POOL:
-            return "VkQueryPool";
-        case VK_OBJECT_TYPE_BUFFER_VIEW:
-            return "VkBufferView";
-        case VK_OBJECT_TYPE_IMAGE_VIEW:
-            return "VkImageView";
-        case VK_OBJECT_TYPE_SHADER_MODULE:
-            return "VkShaderModule";
-        case VK_OBJECT_TYPE_PIPELINE_CACHE:
-            return "VkPipelineCache";
-        case VK_OBJECT_TYPE_PIPELINE_LAYOUT:
-            return "VkPipelineLayout";
-        case VK_OBJECT_TYPE_RENDER_PASS:
-            return "VkRenderPass";
-        case VK_OBJECT_TYPE_PIPELINE:
-            return "VkPipeline";
-        case VK_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT:
-            return "VkDescriptorSetLayout";
-        case VK_OBJECT_TYPE_SAMPLER:
-            return "VkSampler";
-        case VK_OBJECT_TYPE_DESCRIPTOR_POOL:
-            return "VkDescriptorPool";
-        case VK_OBJECT_TYPE_DESCRIPTOR_SET:
-            return "VkDescriptorSet";
-        case VK_OBJECT_TYPE_FRAMEBUFFER:
-            return "VkFramebuffer";
-        case VK_OBJECT_TYPE_COMMAND_POOL:
-            return "VkCommandPool";
-        case VK_OBJECT_TYPE_SURFACE_KHR:
-            return "VkSurfaceKHR";
-        case VK_OBJECT_TYPE_SWAPCHAIN_KHR:
-            return "VkSwapchainKHR";
-        case VK_OBJECT_TYPE_DEBUG_REPORT_CALLBACK_EXT:
-            return "VkDebugReportCallbackEXT";
-        case VK_OBJECT_TYPE_DISPLAY_KHR:
-            return "VkDisplayKHR";
-        case VK_OBJECT_TYPE_DISPLAY_MODE_KHR:
-            return "VkDisplayModeKHR";
-        case VK_OBJECT_TYPE_OBJECT_TABLE_NVX:
-            return "VkObjectTableNVX";
-        case VK_OBJECT_TYPE_INDIRECT_COMMANDS_LAYOUT_NVX:
-            return "VkIndirectCommandsLayoutNVX";
-        case VK_OBJECT_TYPE_DESCRIPTOR_UPDATE_TEMPLATE_KHR:
-            return "VkDescriptorUpdateTemplateKHR";
-        default:
-            return "Unknown Type";
-    }
-}
-
 VKAPI_ATTR VkBool32 VKAPI_CALL debug_messenger_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
                                                         VkDebugUtilsMessageTypeFlagsEXT messageType,
                                                         const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
@@ -573,11 +501,11 @@ VKAPI_ATTR VkBool32 VKAPI_CALL debug_messenger_callback(VkDebugUtilsMessageSever
         for (uint32_t object = 0; object < pCallbackData->objectCount; ++object) {
             if (NULL != pCallbackData->pObjects[object].pObjectName && strlen(pCallbackData->pObjects[object].pObjectName) > 0) {
                 sprintf(tmp_message, "\t\tObject[%d] - %s, Handle %p, Name \"%s\"\n", object,
-                        DebugAnnotObjectToString(pCallbackData->pObjects[object].objectType),
+                        string_VkObjectType(pCallbackData->pObjects[object].objectType),
                         (void *)(pCallbackData->pObjects[object].objectHandle), pCallbackData->pObjects[object].pObjectName);
             } else {
                 sprintf(tmp_message, "\t\tObject[%d] - %s, Handle %p\n", object,
-                        DebugAnnotObjectToString(pCallbackData->pObjects[object].objectType),
+                        string_VkObjectType(pCallbackData->pObjects[object].objectType),
                         (void *)(pCallbackData->pObjects[object].objectHandle));
             }
             strcat(message, tmp_message);
@@ -859,10 +787,7 @@ static void demo_draw_build_cmd(struct demo *demo, VkCommandBuffer cmd_buf) {
         label.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT;
         label.pNext = NULL;
         label.pLabelName = "ActualDraw";
-        label.color[0] = -0.4f;
-        label.color[1] = -0.3f;
-        label.color[2] = -0.2f;
-        label.color[3] = -0.1f;
+        label.color = {-0.4f, -0.3f, -0.2f, -0.1f};
         demo->CmdBeginDebugUtilsLabelEXT(cmd_buf, &label);
     }
 
