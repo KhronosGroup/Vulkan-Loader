a1b5e441b30c9e642ab10cd24d1aa42e712d2c81
Lenny Komow <lenny@lunarg.com>
2019-09-16 16:14:36 -0600

    loader: Add support for metal surfaces
    The adds support for the VK_EXT_metal_surface extension
    
    Change-Id: Ibb1c21e19fb053fe332ea075c3d32de59f7f8881

--- scripts/loader_extension_generator.py
+++ scripts/loader_extension_generator.py
@@ -37,6 +37,7 @@ WSI_EXT_NAMES = ['VK_KHR_surface',
                  'VK_MVK_macos_surface',
                  'VK_MVK_ios_surface',
                  'VK_EXT_headless_surface',
+                 'VK_EXT_metal_surface',
                  'VK_KHR_swapchain',
                  'VK_KHR_display_swapchain',
                  'VK_KHR_get_display_properties2']
