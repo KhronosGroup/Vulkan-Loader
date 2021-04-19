029dc72368772d624aedb9a5abb6e60d27c86a77
Mike Schuchardt <mikes@lunarg.com>
2021-04-19 00:33:45 -0700

    build: Update to header 1.2.176
    - Update known-good
    - Generate source
    - Fix version parsing in FindVulkanHeaders.cmake
    - Add to VK_ENABLE_BETA_EXTENSIONS to GN build to pick up beta
      VkObjectType values
    
    Change-Id: Ied4e4e71017e48d344b1c9cacf037ae989aceec9

--- loader/generated/vk_loader_extensions.h
+++ loader/generated/vk_loader_extensions.h
@@ -299,6 +299,14 @@ struct loader_icd_term_dispatch {
     PFN_vkGetPhysicalDeviceWin32PresentationSupportKHR GetPhysicalDeviceWin32PresentationSupportKHR;
 #endif // VK_USE_PLATFORM_WIN32_KHR
 
+    // ---- VK_KHR_video_queue extension commands
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+    PFN_vkGetPhysicalDeviceVideoCapabilitiesKHR GetPhysicalDeviceVideoCapabilitiesKHR;
+#endif // VK_ENABLE_BETA_EXTENSIONS
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+    PFN_vkGetPhysicalDeviceVideoFormatPropertiesKHR GetPhysicalDeviceVideoFormatPropertiesKHR;
+#endif // VK_ENABLE_BETA_EXTENSIONS
+
     // ---- VK_KHR_get_physical_device_properties2 extension commands
     PFN_vkGetPhysicalDeviceFeatures2KHR GetPhysicalDeviceFeatures2KHR;
     PFN_vkGetPhysicalDeviceProperties2KHR GetPhysicalDeviceProperties2KHR;
