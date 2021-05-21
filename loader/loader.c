d0c6d2101d1f571318ab6978fd725e22d7f58497
Charles Giessen <charles@lunarg.com>
2021-05-20 20:33:16 -0600

    loader: Remove deadstore when breaking from a loop
    Change-Id: Ic72cf396cad918ce255b331fa9a9c3a652b8764d

--- loader/loader.c
+++ loader/loader.c
@@ -4981,7 +4981,6 @@ VkResult loader_create_device_chain(const VkPhysicalDevice pd, const VkDeviceCre
 
                     // Replace the old struct in the pNext chain with this one.
                     pPrev->pNext = (VkBaseOutStructure *)temp_struct;
-                    pNext = (VkBaseOutStructure *)temp_struct;
                 }
                 break;
             }
@@ -5656,7 +5655,6 @@ VKAPI_ATTR VkResult VKAPI_CALL terminator_CreateDevice(VkPhysicalDevice physical
 
                     // Replace the old struct in the pNext chain with this one.
                     pPrev->pNext = (VkBaseOutStructure *)temp_struct;
-                    pNext = (VkBaseOutStructure *)temp_struct;
                 }
                 break;
             }
