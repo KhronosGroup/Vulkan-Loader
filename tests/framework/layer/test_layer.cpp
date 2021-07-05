3adb0474e71b381278d3155c2ac722581a3b6f12
Charles Giessen <charles@lunarg.com>
2021-07-05 17:51:58 -0600

    test: Add Enumeration Function stubs to TestLayer
    Change-Id: I68b3597877fed803f4284e7e67f19ce4e3e2fe53

--- tests/framework/layer/test_layer.cpp
+++ tests/framework/layer/test_layer.cpp
@@ -81,25 +81,69 @@ FRAMEWORK_EXPORT TestLayer* get_new_test_layer_func() {
 }
 }
 
+VKAPI_ATTR VkResult VKAPI_CALL test_vkEnumerateInstanceLayerProperties(uint32_t* pPropertyCount, VkLayerProperties* pProperties) {
+    return VK_SUCCESS;
+}
+VKAPI_ATTR VkResult VKAPI_CALL test_vkEnumerateInstanceExtensionProperties(const char* pLayerName, uint32_t* pPropertyCount,
+                                                                           VkExtensionProperties* pProperties) {
+    return VK_SUCCESS;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL test_vkEnumerateDeviceLayerProperties(VkPhysicalDevice physicalDevice, uint32_t* pPropertyCount,
+                                                                     VkLayerProperties* pProperties) {
+    return VK_SUCCESS;
+}
+VKAPI_ATTR VkResult VKAPI_CALL test_vkEnumerateDeviceExtensionProperties(VkPhysicalDevice physicalDevice, const char* pLayerName,
+                                                                         uint32_t* pPropertyCount,
+                                                                         VkExtensionProperties* pProperties) {
+    return VK_SUCCESS;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL test_vkEnumerateInstanceVersion(uint32_t* pApiVersion) {
+    if (pApiVersion != nullptr) {
+        *pApiVersion = VK_MAKE_VERSION(1, 0, 0);
+    }
+    return VK_SUCCESS;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL test_vkCreateInstance(const VkInstanceCreateInfo* pCreateInfo,
+                                                     const VkAllocationCallbacks* pAllocator, VkInstance* pInstance) {
+    return VK_SUCCESS;
+}
+VKAPI_ATTR void VKAPI_CALL test_vkDestroyInstance(VkInstance instance, const VkAllocationCallbacks* pAllocator) {}
+
+VKAPI_ATTR VkResult VKAPI_CALL test_vkCreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo* pCreateInfo,
+                                                   const VkAllocationCallbacks* pAllocator, VkDevice* pDevice) {
+    return VK_SUCCESS;
+}
+
+// device functions
+
+VKAPI_ATTR void VKAPI_CALL test_vkDestroyDevice(VkDevice device, const VkAllocationCallbacks* pAllocator) {}
+
+// trampolines
+
+#define TO_VOID_PFN(func) reinterpret_cast<PFN_vkVoidFunction>(func)
+
 VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL get_device_func(VkDevice device, const char* pName) {
-    // if (strcmp(pName, "vkDestroyDevice") == 0) return TO_VOID_PFN(test_vkDestroyDevice);
-    // if (strcmp(pName, "vkCreateSwapchainKHR") == 0) return TO_VOID_PFN(test_vkCreateSwapchainKHR);
+    if (string_eq(pName, "vkDestroyDevice")) return TO_VOID_PFN(test_vkDestroyDevice);
 
     return nullptr;
 }
 
 PFN_vkVoidFunction get_instance_func(VkInstance instance, const char* pName) {
     if (pName == nullptr) return nullptr;
-    // if (instance == NULL) {
-    //     if (strcmp(pName, "vkGetInstanceProcAddr") == 0) return TO_VOID_PFN(test_vkGetInstanceProcAddr);
-    //     if (strcmp(pName, "vkEnumerateInstanceExtensionProperties") == 0)
-    //         return TO_VOID_PFN(test_vkEnumerateInstanceExtensionProperties);
-    //     if (strcmp(pName, "vkEnumerateInstanceLayerProperties") == 0)
-    //         return TO_VOID_PFN(test_vkEnumerateInstanceLayerProperties);
-    //     if (strcmp(pName, "vkEnumerateInstanceVersion") == 0)
-    //         return TO_VOID_PFN(test_vkEnumerateInstanceVersion);
-    // }
-    // if (strcmp(pName, "vkGetDeviceProcAddr") == 0) return TO_VOID_PFN(test_vkGetDeviceProcAddr);
+    if (instance == NULL) {
+        if (string_eq(pName, "vkGetInstanceProcAddr")) return TO_VOID_PFN(get_instance_func);
+        if (string_eq(pName, "vkEnumerateInstanceLayerProperties")) return TO_VOID_PFN(test_vkEnumerateInstanceLayerProperties);
+        if (string_eq(pName, "vkEnumerateInstanceExtensionProperties"))
+            return TO_VOID_PFN(test_vkEnumerateInstanceExtensionProperties);
+        if (string_eq(pName, "vkEnumerateInstanceVersion")) return TO_VOID_PFN(test_vkEnumerateInstanceVersion);
+
+        if (string_eq(pName, "vkEnumerateDeviceLayerProperties")) return TO_VOID_PFN(test_vkEnumerateDeviceLayerProperties);
+        if (string_eq(pName, "vkEnumerateDeviceExtensionProperties")) return TO_VOID_PFN(test_vkEnumerateDeviceExtensionProperties);
+    }
+    if (string_eq(pName, "vkGetDeviceProcAddr")) return TO_VOID_PFN(get_device_func);
     return nullptr;
 }
 
@@ -108,24 +152,24 @@ extern "C" {
 #if TEST_LAYER_EXPORT_ENUMERATE_FUNCTIONS
 FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkEnumerateInstanceLayerProperties(uint32_t* pPropertyCount,
                                                                                    VkLayerProperties* pProperties) {
-    return VK_SUCCESS;
+    return test_vkEnumerateInstanceLayerProperties(pPropertyCount, pProperties);
 }
 FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkEnumerateInstanceExtensionProperties(const char* pLayerName,
                                                                                        uint32_t* pPropertyCount,
                                                                                        VkExtensionProperties* pProperties) {
-    return VK_SUCCESS;
+    return test_vkEnumerateInstanceExtensionProperties(pLayerName, pPropertyCount, pProperties);
 }
 
 FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkEnumerateDeviceLayerProperties(VkPhysicalDevice physicalDevice,
                                                                                  uint32_t* pPropertyCount,
                                                                                  VkLayerProperties* pProperties) {
-    return VK_SUCCESS;
+    return test_vkEnumerateDeviceLayerProperties(physicalDevice, pPropertyCount, pProperties);
 }
 FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkEnumerateDeviceExtensionProperties(VkPhysicalDevice physicalDevice,
                                                                                      const char* pLayerName,
                                                                                      uint32_t* pPropertyCount,
                                                                                      VkExtensionProperties* pProperties) {
-    return VK_SUCCESS;
+    return test_vkEnumerateDeviceExtensionProperties(physicalDevice, pLayerName, pPropertyCount, pProperties);
 }
 #endif
 
@@ -171,10 +215,15 @@ FRAMEWORK_EXPORT VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_layerGetPhysicalDev
     return nullptr;
 }
 #endif
+#define LAYER_EXPORT_NEGOTIATE_LOADER_LAYER_INTERFACE_VERSION 1
 
 #if LAYER_EXPORT_NEGOTIATE_LOADER_LAYER_INTERFACE_VERSION
 FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkNegotiateLoaderLayerInterfaceVersion(VkNegotiateLayerInterface* pVersionStruct) {
     if (pVersionStruct) {
+        if (pVersionStruct->loaderLayerInterfaceVersion < layer.min_implementation_version) {
+            return VK_ERROR_INITIALIZATION_FAILED;
+        }
+
         pVersionStruct->loaderLayerInterfaceVersion = layer.implementation_version;
         pVersionStruct->pfnGetInstanceProcAddr = get_instance_func;
         pVersionStruct->pfnGetDeviceProcAddr = get_device_func;
@@ -183,6 +232,7 @@ FRAMEWORK_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkNegotiateLoaderLayerInterfaceV
 #else
         pVersionStruct->pfnGetPhysicalDeviceProcAddr = nullptr;
 #endif
+
         return VK_SUCCESS;
     }
     return VK_ERROR_INITIALIZATION_FAILED;
