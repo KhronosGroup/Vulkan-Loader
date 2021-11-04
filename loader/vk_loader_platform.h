b8ee23e4d5ac9214de27056eb0a71725e89190c0
Eleni Maria Stea <elene.mst@gmail.com>
2021-11-04 12:13:21 +0200

    loader, asm_offset: define BSD_VISIBLE and fix ifdef bug
    In vk_loader_platform.h there was a leftover #if defined (__FreeBSD__)
    ... elif #defined(__FreeBSD__) that needed fixing. After that it was
    necessary to define BSD_VISIBLE before including <sys/types.h> required
    by <sys/sysctl.h> required by asm_offset and loader CMakeLists.txt had
    to be  modified accordingly.

--- loader/vk_loader_platform.h
+++ loader/vk_loader_platform.h
@@ -156,7 +156,7 @@ static inline bool loader_platform_is_path_absolute(const char *path) {
 
 static inline char *loader_platform_dirname(char *path) { return dirname(path); }
 
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__)
 
 // find application path + name. Path cannot be longer than 1024, returns NULL if it is greater than that.
 static inline char *loader_platform_executable_path(char *buffer, size_t size) {
