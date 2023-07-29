b308c698008b1025a8f1d616543d03c54287e600
Charles Giessen <charles@lunarg.com>
2023-07-29 13:15:13 -0600

    Fixed the value of the FATAL_ERROR bit
    0x160 is *not* the next highest power of two in hex after 0x80, it is in fact 0x100.
    
    This error caused the log function to always log every message, rather than just
    fatal ones and whatever messages match the VK_LOADER_LOG environment variable.

--- loader/log.c
+++ loader/log.c
@@ -157,9 +157,9 @@ void loader_log(const struct loader_instance *inst, VkFlags msg_type, int32_t ms
         // Exit early if the current instance settings do not ask for logging to stderr
         if (inst && inst->settings.settings_active && 0 == (msg_type & inst->settings.debug_level)) {
             return;
-        } else {
             // Check the global settings and if that doesn't say to skip, check the environment variable
-            if (0 == (msg_type & g_loader_debug)) return;
+        } else if (0 == (msg_type & g_loader_debug)) {
+            return;
         }
     }
 
