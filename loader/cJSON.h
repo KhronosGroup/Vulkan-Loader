1529ba4e4b0efabbf01e1cc349609af2cde11993
Charles Giessen <charles@lunarg.com>
2023-12-07 13:41:31 -0700

    Fix spelling mistakes in comments
    These creep in over time, and make the codebase harder to understand due to spelling
    errors causing confusion when reading comments.

--- loader/cJSON.h
+++ loader/cJSON.h
@@ -105,7 +105,7 @@ VkResult loader_parse_json_string_to_existing_str(const struct loader_instance *
 // It is the callers responsibility to free out_string.
 VkResult loader_parse_json_string(cJSON *object, const char *key, char **out_string);
 
-// Given a cJSON object, find the array of strings assocated with they key and writes the count into out_count and data into
+// Given a cJSON object, find the array of strings associated with they key and writes the count into out_count and data into
 // out_array_of_strings. It is the callers responsibility to free out_array_of_strings.
 VkResult loader_parse_json_array_of_strings(const struct loader_instance *inst, cJSON *object, const char *key,
                                             struct loader_string_list *string_list);
