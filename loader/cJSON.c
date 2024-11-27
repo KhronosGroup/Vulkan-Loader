32b028d83bcdfa00071f41dd903c66dd039811d3
Charles Giessen <charles@lunarg.com>
2024-11-26 20:14:32 -0600

    Use snprintf in updated cJSON

--- loader/cJSON.c
+++ loader/cJSON.c
@@ -392,21 +392,21 @@ static cJSON_bool print_number(const cJSON *const item, printbuffer *const outpu
 
     /* This checks for NaN and Infinity */
     if (isnan(d) || isinf(d)) {
-        length = sprintf((char *)number_buffer, "null");
+        length = snprintf((char *)number_buffer, 26, "null");
     } else if (d == (double)item->valueint) {
-        length = sprintf((char *)number_buffer, "%d", item->valueint);
+        length = snprintf((char *)number_buffer, 26, "%d", item->valueint);
     } else {
         /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-        length = sprintf((char *)number_buffer, "%1.15g", d);
+        length = snprintf((char *)number_buffer, 26, "%1.15g", d);
 
         /* Check whether the original double can be recovered */
         if ((sscanf((char *)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d)) {
             /* If not, print with 17 decimal places of precision */
-            length = sprintf((char *)number_buffer, "%1.17g", d);
+            length = snprintf((char *)number_buffer, 26, "%1.17g", d);
         }
     }
 
-    /* sprintf failed or buffer overrun occurred */
+    /* snprintf failed or buffer overrun occurred */
     if ((length < 0) || (length > (int)(sizeof(number_buffer) - 1))) {
         return false;
     }
@@ -772,7 +772,7 @@ static cJSON_bool print_string_ptr(const unsigned char *const input, printbuffer
                     break;
                 default:
                     /* escape and print as unicode codepoint */
-                    sprintf((char *)output_pointer, "u%04x", *input_pointer);
+                    snprintf((char *)output_pointer, output_length - (size_t)(output_pointer - output), "u%04x", *input_pointer);
                     output_pointer += 4;
                     break;
             }
