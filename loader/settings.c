9cb32848b88674cbe0fc9883b5afc702ae3a5f42
Charles Giessen <charles@lunarg.com>
2025-08-02 16:43:16 -0500

    Print deviceUUID correctly by using %02x

--- loader/settings.c
+++ loader/settings.c
@@ -606,7 +606,8 @@ void log_settings(const struct loader_instance* inst, loader_settings* settings)
         for (uint32_t i = 0; i < settings->device_configuration_count; i++) {
             loader_log(inst, VULKAN_LOADER_DEBUG_BIT, 0, "---- Device Configuration [%d] ----", i);
             uint8_t* id = settings->device_configurations[i].deviceUUID;
-            loader_log(inst, VULKAN_LOADER_DEBUG_BIT, 0, "deviceUUID: %x%x%x%x-%x%x-%x%x-%x%x-%x%x%x%x%x%x", id[0], id[1], id[2],
+            loader_log(inst, VULKAN_LOADER_DEBUG_BIT, 0,
+                       "deviceUUID: %02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x", id[0], id[1], id[2],
                        id[3], id[4], id[5], id[6], id[7], id[8], id[9], id[10], id[11], id[12], id[13], id[14], id[15]);
         }
     }
